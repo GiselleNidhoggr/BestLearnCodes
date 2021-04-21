536267677eb824a17665f154f89a6c57608c83bd
Nidhoggr <64610664+GiselleOAlmeida@users.noreply.github.com>
2021-04-21 11:47:03 -0300

    Add files via upload

---
+++ Função Aula 09.cpp
@@ -0,0 +1,107 @@
+// Função Aula 09.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <istream>
+
+using namespace std;
+
+
+
+//f(x)=x^2-3x+5
+//int main()
+//{
+  //  float a;
+  //  cout << "Informe um numero: \n";
+  //  cin >> a;
+  // float funca = ((pow(a,2)) - (3 * a) + 5);
+  //cout << "Seu numero da sorte é: " << funca << endl;
+//}
+
+//3 notas de um aluno
+//media da turma
+//calcula a nota media do aluno
+//informa se eh acima ou abaixo da media da sala
+//int main() {
+	//float mediasala, nota1, nota2, nota3, notamedia ; 
+	//cout << "Informe a media da sala ";
+	//cin >> mediasala;
+	//cout << "Informe a nota 1 ";
+	//cin >> nota1;
+	//cout << "Informe a nota 2 ";
+	//cin >> nota2;
+	//cout << "Informe a nota 3 ";
+	//cin >> nota3;
+	//notamedia = ((nota1 + nota2 + nota3) / 3);
+	//cout << "A sua media foi: \n" << notamedia;
+	//if (notamedia >= mediasala) {
+	//	cout << "\nSua media foi acima que a da sala\n";
+	//}
+	//else{
+	//	cout<<"\nSua media foi abaixo que a da sala";
+	//}
+//}
+
+//informar um número e calcular o fatoria
+//int main() {
+	//float n, aux , prod=1 ;
+	//cout << "Insira um numero: ";
+	//cin >> n;
+	//for (aux = 1; aux <= n; aux++)
+		//prod *= aux;
+	//cout << "\nO fatorial do numero eh : " << prod;
+//}
+
+//calculo da potencia de B^e
+//pergunta ao usuario se quer calcular outra potencia (s/n)
+//sem pow
+//float potencia(float x, int y) {
+	//float pot = 1;
+	//int cont = 1;
+	//while (cont <= y) {
+	//	pot *= x;
+	//	cont++;
+	//}
+	//return pot;
+//}
+//
+//int main() {
+	//float b;
+	//int e;
+	//while (1) {
+	//	cout << "Digite a base (numero real)\n";
+	//	cin >> b;
+	//	cout << "Digite o expoente (numero natural)\n";
+	//	cin >> e;
+	//	float respot = potencia(b, e);
+	//	cout << "O resultado de " << b << " elevado a " << e << " e igual a " << respot << endl;
+	//	int q;
+	//	cout << "Digite 1 se quiser calcular outra potencia e 0 se não quiser\n";
+	//	cin >> q;
+	//	if (q == 0) {
+	//		break;
+	//	}
+	//}
+//
+	//return 0;
+//}
+
+//e: && ou || não !
+//maior de > 21 anos e com renda abaixo de 1200 aprovadas
+//pergunta a idade e renda
+//informa se participa do programa ou não
+//int main() {
+	//int idade;
+	//float salario;
+	//cout << "Informe sua idade:";
+	//cin >> idade;
+	//cout << "Informe seu salario:";
+	//cin >> salario;
+	//if(idade>21&&salario<1200){
+		//cout << "Voce pode participar do programa minha casa de probre";
+	//}
+	//else {
+	//	cout << "Voce nao foi aprovado no programa minha casa minha bida";
+	//}
+//}
+
